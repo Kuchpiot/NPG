7573f89e1737dd851f2a46be794dfced3723be4b
Kuchpiot <kucharski@student.agh.edu.pl>
2016-11-23 15:46:22 +0100

    osmy

---
+++ 1 - Kopia (8).c
@@ -0,0 +1,97 @@
+#include <iostream>
+
+int main()
+{
+	cout << "Linia" << endl;
+cout << "Linia" << endl;
+
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;cout << "Linia" << endl;
+
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;cout << "Linia" << endl;
+cout << "Linia" << endl;
+cout << "Linia" << endl;
+
+return 0;
+}
